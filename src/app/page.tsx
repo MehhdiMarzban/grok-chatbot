7640636774b1329eb5591c7f4df840994059a858
Mehdi Marzban <marzban7272@gmail.com>
2025-03-07 21:02:46 +0330

    Enhance Chat component styling and layout for improved user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,23 @@ export default function Home() {
     const { messages, input, handleInputChange, handleSubmit, isLoading, stop, append } = useChat();
     return (
         <div className="flex items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <Chat
-                className="grow"
-                messages={messages}
-                handleSubmit={handleSubmit}
-                input={input}
-                handleInputChange={handleInputChange}
-                isGenerating={isLoading}
-                stop={stop}
-                append={append}
-                suggestions={[
-                    "Generate a tasty vegan lasagna recipe for 3 people.",
-                    "Generate a list of 5 questions for a job interview for a software engineer.",
-                    "Who won the 2022 FIFA World Cup?",
-                ]}
-            />
+            <div className="border rounded-md p-4 justify-center mx-auto">
+                <Chat
+                    className="grow space-y-4 h-96 max-w-4xl w-full"
+                    messages={messages}
+                    handleSubmit={handleSubmit}
+                    input={input}
+                    handleInputChange={handleInputChange}
+                    isGenerating={isLoading}
+                    stop={stop}
+                    append={append}
+                    suggestions={[
+                        "Generate a tasty vegan lasagna recipe for 3 people.",
+                        "Generate a list of 5 questions for a job interview for a software engineer.",
+                        "Who won the 2026 FIFA World Cup?",
+                    ]}
+                />
+            </div>
         </div>
     );
 }
